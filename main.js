a89edbd24f38f7bb5b3f294d3d4f086185da5e86
Abel <abelastra0@gmail.com>
2024-04-22 13:57:34 -0300

    fusion del menu en mobile

--- main.js
+++ main.js
@@ -1,10 +1,18 @@
 const menuEmail = document.querySelector('.navbar-email')
 const desktopMenu = document.querySelector('.desktop-menu')
 
+const menuHam = document.querySelector('.menu')
+const mobileMenu = document.querySelector('.mobile-menu')
+
 
 menuEmail.addEventListener('click',toggleDesktopMenu);
+menuHam.addEventListener('click',toggleHamMenu);
 
 
  function toggleDesktopMenu(){
   desktopMenu.classList.toggle('inactive')
+ }
+
+ function toggleHamMenu(){
+    mobileMenu.classList.toggle('inactive')
  }
