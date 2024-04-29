cee0c1d9c90ed560c28cdf9ad8a9dd048aa471cd
Abel <abelastra0@gmail.com>
2024-04-29 20:04:47 -0300

    se agrego el carrito de compras

--- main.js
+++ main.js
@@ -4,15 +4,34 @@ const desktopMenu = document.querySelector('.desktop-menu')
 const menuHam = document.querySelector('.menu')
 const mobileMenu = document.querySelector('.mobile-menu')
 
+const carritoDeCompras = document.querySelector('.navbar-shopping-cart')
+const productos =document.querySelector('.product-detail')
 
 menuEmail.addEventListener('click',toggleDesktopMenu);
-menuHam.addEventListener('click',toggleHamMenu);
+
+menuHam.addEventListener('click',toggleHamMenu);                                
+
+carritoDeCompras.addEventListener('click',toggleProductos)
 
 
  function toggleDesktopMenu(){
+   mobileMenu.classList.add('inactive')
+   productos.classList.add('inactive')
+
   desktopMenu.classList.toggle('inactive')
  }
 
  function toggleHamMenu(){
+   productos.classList.add('inactive')
+
     mobileMenu.classList.toggle('inactive')
+ }
+
+ function toggleProductos(){
+   
+   desktopMenu.classList.add('inactive')
+   mobileMenu.classList.add('inactive')
+
+   productos.classList.toggle('inactive')
+
  }
